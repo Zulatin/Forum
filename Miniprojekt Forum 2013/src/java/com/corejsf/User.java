d8712536d9206c0050ef06b4772bbb8b392f7134
Casper <Casper@10.10.135.98>
2013-10-29 10:03:42 +0100

    UserClass Done

--- Miniprojekt Forum 2013/src/java/com/corejsf/User.java
+++ Miniprojekt Forum 2013/src/java/com/corejsf/User.java
@@ -6,10 +6,61 @@
 
 package com.corejsf;
 
-/**
- *
- * @author Tobias
- */
 public class User {
+    private int age;
+    private String username;
+    private int passeword;
+    private String name;
+    private boolean admin=false;
+    
+     public User(String name, int age, String username, int password){
+        this.name=name;
+        this.age=age;
+        this.username=username;
+        this.passeword=password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getPasseword() {
+        return passeword;
+    }
+
+    public void setPasseword(int passeword) {
+        this.passeword = passeword;
+    }
+     public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+    
+    
+   
     
 }
+
